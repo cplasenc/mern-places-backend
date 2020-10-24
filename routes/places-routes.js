ea231e5213b2d5d3da4c77f6938a907887e0e39a
C. Plasencia <36932780+ctnplasencia@users.noreply.github.com>
2020-10-24 19:47:50 +0100

    añadidos controladores

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,50 +1,12 @@
 const express = require('express');
-
+const placesController = require('../controller/places-controller');
 const HttpError = require('../model/http-error');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        location: {
-            lat: 40.7484474,
-            lng: -73.9871516
-        },
-        address: '20 W 34th St, New York, NY 10001',
-        creator: 'u1'
-    }
-];
 
+router.get('/:pid', placesController.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.id === placeId;
-    });
-
-    if (!place) {
-        //return res.status(404).json({ message: 'No se ha encontado ningún lugar' });
-        return next(new HttpError('No se ha encontrado ningún lugar', 404));
-    }
-
-    res.json({ place });
-});
-
-router.get('user/:uid', (req, res, next) => {
-    const userId = req.params.uid;
-    const place = DUMMY_PLACES.find(p => {
-        return p.creator === userId;
-    });
-
-    if (!place) {
-        //return res.status(404).json({ message: 'No se ha encontrado ningún lugar' });
-        return next(new HttpError('No se ha encontrado ningún lugar', 404));
-    }
-
-    res.json({ place });
-});
+router.get('user/:uid', placesController.getPlaceByUserId);
 
 module.exports = router;
