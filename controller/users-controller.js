cd9cdf3e0f21c21a55fcb827d79a0ad91e4e20f2
C. Plasencia <36932780+ctnplasencia@users.noreply.github.com>
2020-11-16 19:08:02 +0000

    añadir (POST)

--- controller/users-controller.js
+++ controller/users-controller.js
@@ -68,7 +68,7 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
-    res.json({message: 'Sesión iniciada'});
+    res.json({message: 'Sesión iniciada', user: existingUser.toObject({ getters: true })});
 };
 
 exports.getUsers = getUsers;
