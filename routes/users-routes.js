c1093a1ebb515452a3240a7a70d32cb3ea1f3454
Cristian Plasencia <plc.cristian@gmail.com>
2020-12-25 20:56:33 +0000

    usando multer para guardar imagenes

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const usersController = require('../controller/users-controller');
 const { check } = require('express-validator');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
-router.post('/signup',
+router.post(
+    '/signup',
+    fileUpload.single('image'),
     [
         check('name').not().isEmpty(),
         check('email').normalizeEmail().isEmail(),
